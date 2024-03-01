8357cc244edd9a30f6d9f62b86c15da456c4a201
benjsmyth <smythjben@gmail.com>
2024-03-01 13:27:04 -0500

    Fix typo

--- c1/cminus-package/absyn/WhileExp.java
+++ c1/cminus-package/absyn/WhileExp.java
@@ -4,14 +4,14 @@ public class WhileExp extends Exp {
     
     public Exp test;
     public Exp body;
-    
+    public Exp thenpart;
 
     public WhileExp( int row, int col, Exp test, Exp body){
     
         this.row = row;
         this.col = col;
         this.test = test;
-        this.then = body;
+        this.thenpart = body;
     }
 
   public void accept( AbsynVisitor visitor, int level ) {

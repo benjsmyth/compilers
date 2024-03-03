0fe875d99f435694ebb5d95c410a6c6531f2cfa6
Eli Daniels <danielse@linux.socs.uoguelph.ca>
2024-03-03 08:54:10 -0500

    printing out opExp now prints the symbol

--- c1/cminus-package/ShowTreeVisitor.java
+++ c1/cminus-package/ShowTreeVisitor.java
@@ -130,7 +130,69 @@ public class ShowTreeVisitor implements AbsynVisitor {
     
     if (opExp.op != -1){
       indent( level );
-      System.out.println(opExp.op);
+      //System.out.println(opExp.op);
+      switch (opExp.op){
+
+        case 0:
+          System.out.println("+");
+        break;
+
+        case 1:
+          System.out.println("-");
+        break;
+
+        case 2:
+          System.out.println("uminus");
+        break;
+
+        case 3:
+          System.out.println("*");
+        break;
+
+        case 4:
+          System.out.println("/");
+        break;
+
+        case 5:
+          System.out.println("=");
+        break;
+
+        case 6:
+          System.out.println("!=");
+        break;
+
+        case 7:
+          System.out.println("<");
+        break;
+
+        case 8:
+          System.out.println("<=");
+        break;
+
+        case 9:
+          System.out.println(">");
+        break;
+
+        case 10:
+          System.out.println(">=");
+        break;
+
+        case 11:
+          System.out.println("~");
+        break;
+
+        case 12:
+          System.out.println("&&");
+        break;
+
+        case 13:
+          System.out.println("||");
+        break;
+
+        default:
+        System.out.println("Invalid Operation");
+
+      }
     }
 
     if (opExp.right != null)

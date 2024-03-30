cb6ac9035d3c2bc11ca3600a01874724188f1660
benjsmyth <smythjben@gmail.com>
2024-03-30 05:23:01 -0400

    Add prelude

--- c3/cminus-package/CodeGenerator.java
+++ c3/cminus-package/CodeGenerator.java
@@ -1,21 +1,235 @@
 import absyn.*;
+import java.io.PrintStream;
 
 public class CodeGenerator implements AbsynVisitor {
+  int mainEntry, globalOffset;
+  PrintStream console, stream;
+  public static boolean valid;
 
-  /* Utility methods */
+  private static int ac = 0;
+  private static int fp = 5;
+  private static int gp = 6;
+  private static int pc = 7;
+
+  public CodeGenerator(PrintStream console, PrintStream stream) {
+    this.console = console;
+    this.stream = stream;
+    this.valid = true;
+    this.prelude();
+  }
+
+  private void prelude() {
+    System.out.println(String.format("0: LD %d, 0(%d)",
+      this.gp, this.ac));  // Load global pointer with maxaddress
+    System.out.println(String.format("1: LDA %d, 0(%d)",
+      this.fp, this.gp));  // Copy global pointer to frame pointer
+    System.out.println(String.format("2: ST %d, 0(%d)",
+      this.ac, this.ac));  // Clear data address 0
+  }
+
+  private void finale() {
+    //
+  }
+
+  public void printError(String err) {
+    SemanticAnalyzer.valid = false;
+    System.setOut(this.console);
+    System.err.println(err);
+    System.setOut(this.stream);
+  }
+
+  // Emitting routines
   private void emitCode(String code) {
-    // Not sure what this does. Propagate to parent tree somehow?
+    System.out.println(code);
   }
   private void emitError(String message, int row, int col) {
     System.err.println(String.format("Error: %s at row %d, column %d",
       message, row, col)
     );
   }
-  private String newTemp(int n) {
-    return String.format("t%d", n);
+
+  // Visitor wrapper
+  public void visit(Absyn tree) {
+    this.prelude();
+    // Generate I/O routines
+    tree.accept(this, 0, false);
+    this.finale();
   }
 
-  /* Code generation methods */
+  // Visitor methods
+  public void visit(ArrayDec arrayDec, int level, boolean isAddr) {
+    level++;
+    switch (arrayDec.typ.type){
+      case 0:
+        // System.out.println("type: BOOL");
+        break;
+      case 1:
+        // System.out.println("type: INT");
+        break;
+      case 2:
+        // System.out.println("type: VOID");
+        break;
+      default:
+        // System.err.println("Error: Invalid Typing");
+        break;
+    }
+    // System.out.println("name: " + arrayDec.name);
+    // System.out.println("size: " + arrayDec.size);
+  }
+
+  public void visit(AssignExp assignExp, int level, boolean isAddress) {
+    level++;
+    if (assignExp.lhs != null)
+      assignExp.lhs.accept(this, level, isAddress);
+    if (assignExp.rhs != null)
+      assignExp.rhs.accept(this, level, isAddress);
+  }
+
+  public void visit(BoolExp boolExp, int level, boolean isAddress) {
+    level++;
+    System.out.println("value: " + boolExp.value);
+  }
+
+  public void visit(CallExp callExp, int level, boolean isAddress) {
+    level++;
+    // System.out.println("function name: " + callExp.func);
+    if (callExp.args != null)
+      callExp.args.accept(this, level, isAddress);
+  }
+
+  public void visit(CompoundExp compoundExp, int level, boolean isAddress) {
+    level++;
+    if (compoundExp.decs != null)
+      compoundExp.decs.accept(this, level, isAddress);
+    if (compoundExp.exps != null)
+      compoundExp.exps.accept(this, level, isAddress);
+  }
+
+  public void visit(DecList decList, int level, boolean isAddress) {
+    level++;
+    while(decList != null) {
+      if (decList.head != null)
+        decList.head.accept(this, level, isAddress);
+      decList = decList.tail;
+    }
+  }
+
+  public void visit(ExpList expList, int level, boolean isAddress) {
+    level++;
+    while(expList != null) {
+      if (expList.head != null)
+        expList.head.accept(this, level, isAddress);
+      expList = expList.tail;
+    }
+  }
+
+  public void visit(FunctionDec functionDec, int level, boolean isAddress) {
+    level++;
+    if (functionDec.params != null)
+      functionDec.params.accept(this, level, isAddress);
+    if (functionDec.body != null)
+      functionDec.body.accept(this, level, isAddress);
+  }
+
+  public void visit(IfExp ifExp, int level, boolean isAddress) {
+    level++;
+    if (ifExp.test != null)
+      ifExp.test.accept(this, level, isAddress);
+    if (ifExp.thenpart != null)
+      ifExp.thenpart.accept(this, level, isAddress);
+    if (ifExp.elsepart != null) {
+       System.out.println("ELSE");
+       ifExp.elsepart.accept(this, level, isAddress);
+    }
+  }
+
+  public void visit(IndexVar indexVar, int level, boolean isAddress) {
+    level++;
+    System.out.println("name: " + indexVar.name);
+    indexVar.index.accept(this, level, isAddress);
+  }
+
+  public void visit(IntExp intExp, int level, boolean isAddress) {
+    level++;
+    System.out.println("value: " + intExp.value);
+  }
+
+  public void visit(NameTy nameTy, int level, boolean isAddress) {
+    level++;
+    switch(nameTy.type) {
+      case 0:
+        // System.out.println("type: BOOL");
+        break;
+      case 1:
+        // System.out.println("type: INT");
+        break;
+      case 2:
+        // System.out.println("type: VOID");
+        break;
+      default:
+        // System.err.println("Error: Invalid Type");
+        break;
+    }
+  }
+
+  public void visit(NilExp nilExp, int level, boolean isAddress) {
+  }
+
+  public void visit(OpExp opExp, int level, boolean isAddress) {
+    level++;
+    String code = "";
+    if (opExp.left != null)
+      opExp.left.accept(this, level, isAddress);
+    if (opExp.op != -1) {}
+    System.out.println();
+    if (opExp.right != null)
+      opExp.right.accept(this, level, isAddress);
+  }
+
+  public void visit(ReturnExp returnExp, int level, boolean isAddress) {
+    level++;
+    if (returnExp != null)
+      returnExp.exp.accept(this, level, isAddress);
+  }
+
+  public void visit(SimpleDec simpleDec, int level, boolean isAddress) {
+    level++;
+    if (simpleDec.typ != null)
+      simpleDec.typ.accept(this, level, isAddress);
+    // System.out.println("name: " + simpleDec.name);
+  }
+
+  public void visit(SimpleVar simpleVar, int level, boolean isAddress) {
+    level++;
+    // System.out.println("name: " + simpleVar.name);
+  }
+
+  public void visit(VarDecList varDecList, int level, boolean isAddress) {
+    level++;
+    while(varDecList != null) {
+      if (varDecList.head != null)
+        varDecList.head.accept(this, level, isAddress);
+      varDecList = varDecList.tail;
+    }
+  }
+
+  public void visit(VarExp varExp, int level, boolean isAddress) {
+    level++;
+    if (varExp.variable != null)
+      varExp.variable.accept(this, level, isAddress);
+  }
+
+  public void visit(WhileExp whileExp, int level, boolean isAddress) {
+    level++;
+    if (whileExp.test != null)
+      whileExp.test.accept(this, level, isAddress);
+    if (whileExp.body != null)
+      whileExp.body.accept(this, level, isAddress);
+  }
+}
+
+  //private String newTemp(int n) {return String.format("t%d", n);}
+  /* Code generation methods
   private void genCode(Exp tree) {
     if (tree != null) {
       String code;
@@ -51,231 +265,8 @@ public class CodeGenerator implements AbsynVisitor {
         assignTree.temp = assignTree.lhs.temp;
         code = String.format("%s = %s", assignTree.lhs.temp, assignTree.rhs.temp);
         emitCode(code);
-      } 
-        else if (tree instanceof IntExp) {/* Do nothing */}
+      }
+        else if (tree instanceof IntExp) {}
         else emitError("Could not generate expression", 0, 0);
-      
     }
-  }
-
-  /* Visitor methods */
-  public void visit(ArrayDec arrayDec, int level, boolean flag) {
-
-    System.out.println("Generating code for ArrayDec:");
-    level++;
-    switch (arrayDec.typ.type){
-      case 0:
-        // System.out.println("type: BOOL");
-        break;
-      case 1:
-        // System.out.println("type: INT");
-        break;
-      case 2:
-        // System.out.println("type: VOID");
-        break;
-      default:
-        // System.err.println("Error: Invalid Typing");
-        break;
-    }
-    
-    // System.out.println("name: " + arrayDec.name);
-    
-    // System.out.println("size: " + arrayDec.size);
-  }
-  
-  public void visit(AssignExp assignExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for AssignExp:");
-    level++;
-    if (assignExp.lhs != null)
-      assignExp.lhs.accept(this, level, flag);
-    if (assignExp.rhs != null)
-      assignExp.rhs.accept(this, level, flag);
-  }
-
-  public void visit(BoolExp boolExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for BoolExp:");
-    level++;
-    
-    System.out.println("value: " + boolExp.value);
-  }
-
-  public void visit(CallExp callExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for CallExp:");
-    level++;
-    
-    // System.out.println("function name: " + callExp.func);
-    if (callExp.args != null)
-      callExp.args.accept(this, level, flag);
-  }
-
-  public void visit(CompoundExp compoundExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for CompoundExp:");
-    level++;
-    if (compoundExp.decs != null)
-      compoundExp.decs.accept(this, level, flag);
-    if (compoundExp.exps != null)
-      compoundExp.exps.accept(this, level, flag);
-  }
-
-  public void visit(DecList decList, int level, boolean flag) {
-    System.out.println("Generating code for DecList:");
-    level++;
-    while(decList != null) {
-      if (decList.head != null)
-        decList.head.accept(this, level, flag);
-      decList = decList.tail;
-    } 
-  }
-
-  public void visit(ExpList expList, int level, boolean flag) {
-    
-    System.out.println("Generating code for ExpList:");
-    level++;
-    while(expList != null) {
-      if (expList.head != null)
-        expList.head.accept(this, level, flag);
-      expList = expList.tail;
-    } 
-  }
-  
-
-  public void visit(FunctionDec functionDec, int level, boolean flag) {
-    
-    System.out.println("Generating code for FunctionDec:");
-    level++;
-    if (functionDec.params != null)
-      functionDec.params.accept(this, level, flag);
-    if (functionDec.body != null)
-      functionDec.body.accept(this, level, flag);
-  }
-
-  public void visit(IfExp ifExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for IfExp:");
-    level++;
-    if (ifExp.test != null)
-      ifExp.test.accept(this, level, flag);
-    if (ifExp.thenpart != null)
-      ifExp.thenpart.accept(this, level, flag);
-    if (ifExp.elsepart != null) {
-       
-       System.out.println("ELSE");
-       ifExp.elsepart.accept(this, level, flag);
-    }
-  }
-
-  public void visit(IndexVar indexVar, int level, boolean flag) {
-    
-    System.out.println("Generating code for IndexVar:");
-    level++;
-    
-    System.out.println("name: " + indexVar.name);
-    indexVar.index.accept(this, level, flag);
-  }
-
-  public void visit(IntExp intExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for IntExp:");
-    level++;
-    
-    System.out.println("value: " + intExp.value);
-  }
-
-  public void visit(NameTy nameTy, int level, boolean flag) {
-    
-    System.out.println("Generating code for NameTy:");
-    level++;
-    
-    switch(nameTy.type) {
-      case 0:
-        // System.out.println("type: BOOL");
-        break;
-      case 1:
-        // System.out.println("type: INT");
-        break;
-      case 2:
-        // System.out.println("type: VOID");
-        break;
-      default:
-        // System.err.println("Error: Invalid Type");
-        break;
-    }
-  }
-
-  public void visit(NilExp nilExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for NilExp:");
-  }
-
-  public void visit(OpExp opExp, int level, boolean flag) {
-    level++;
-    String code = "";
-    if (opExp.left != null)
-      opExp.left.accept(this, level, flag);
-    if (opExp.op != -1) {
-      
-    }
-    System.out.println();
-    if (opExp.right != null)
-      opExp.right.accept(this, level, flag);
-  }
-
-  public void visit(ReturnExp returnExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for ReturnExp:");
-    level++;
-    if (returnExp != null)
-      returnExp.exp.accept(this, level, flag);
-  }
-
-  public void visit(SimpleDec simpleDec, int level, boolean flag) {
-    
-    System.out.println("Generating code for SimpleDec:");
-    level++;
-    if (simpleDec.typ != null)
-      simpleDec.typ.accept(this, level, flag);
-    
-    // System.out.println("name: " + simpleDec.name);
-  }
-
-  public void visit(SimpleVar simpleVar, int level, boolean flag) {
-    
-    System.out.println("Generating code for SimpleVar:");
-    level++;
-    
-    // System.out.println("name: " + simpleVar.name);
-  }
-
-  public void visit(VarDecList varDecList, int level, boolean flag) {
-    
-    System.out.println("Generating code for VarDecList:");
-    level++;
-    while(varDecList != null) {
-      if (varDecList.head != null)
-        varDecList.head.accept(this, level, flag);
-      varDecList = varDecList.tail;
-    } 
-  }
-
-  public void visit(VarExp varExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for VarExp:");
-    level++;
-    if (varExp.variable != null)
-      varExp.variable.accept(this, level, flag);
-  }
-
-  public void visit(WhileExp whileExp, int level, boolean flag) {
-    
-    System.out.println("Generating code for WhileExp:");
-    level++;
-    if (whileExp.test != null)
-      whileExp.test.accept(this, level, flag);
-    if (whileExp.body != null)
-      whileExp.body.accept(this, level, flag);
-  }
-}
+  }*/
